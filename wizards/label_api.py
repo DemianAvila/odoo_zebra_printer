af72fd7c55948eeb2efc852248abb5820a2595eb
root <root@localhost.localdomain>
2022-10-11 22:41:37 -0500

    Correct formating of zpl label

--- wizards/label_api.py
+++ wizards/label_api.py
@@ -16,15 +16,17 @@ l = zpl.Label(data["total_width"], data["total_height"])
 for index, ticket in enumerate(data["tickets"]):
     #CENTER
     center = (((l.width/2) - data["partial_height"])/2)+ticket["origin"][0]
-    l.origin(center, ticket["origin"][1])
+    l.origin(center, ticket["origin"][1]+1)
     #TEXT
     l.write_text(ticket["product"], char_height=2, char_width=2)
     l.endorigin()
     #BARCODE
+    barcode = Image.open(f"/tmp/barcode{index}.jpeg")
+    center = (((l.width/2))/2)+ticket["origin"][0]
     with open(f"/tmp/barcode{index}.jpeg", "wb") as f:
         Code128(ticket["lot"], writer=ImageWriter()).write(f)
-    l.origin(center, ticket["origin"][1]+2.5)
-    l.write_graphic(Image.open(f"/tmp/barcode{index}.jpeg"), 20)
+    l.origin(center, ticket["origin"][1]+3)
+    l.write_graphic(barcode, 20)
     l.endorigin()
 
 file = open("/tmp/zpl_label.txt", "w")
