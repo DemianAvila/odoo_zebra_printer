2c67f12c99c9f4448572a3c3bc6e44fd5077caaf
root <root@localhost.localdomain>
2022-10-12 08:25:07 -0500

    Correctly generated pdf with labels

--- wizards/label_api.py
+++ wizards/label_api.py
@@ -1,6 +1,9 @@
 import zpl 
 import json
 import os
+import shutil
+#import subprocess
+import requests
 from PIL import Image
 from io import BytesIO
 from barcode import Code128
@@ -21,14 +24,41 @@ for index, ticket in enumerate(data["tickets"]):
     l.write_text(ticket["product"], char_height=2, char_width=2)
     l.endorigin()
     #BARCODE
-    barcode = Image.open(f"/tmp/barcode{index}.jpeg")
     center = (((l.width/2))/2)+ticket["origin"][0]
     with open(f"/tmp/barcode{index}.jpeg", "wb") as f:
         Code128(ticket["lot"], writer=ImageWriter()).write(f)
+    barcode = Image.open(f"/tmp/barcode{index}.jpeg")
     l.origin(center, ticket["origin"][1]+3)
     l.write_graphic(barcode, 20)
     l.endorigin()
 
-file = open("/tmp/zpl_label.txt", "w")
-file.write(l.dumpZPL())
-file.close()
+#file = open("/tmp/zpl_label.zpl", "w")
+#file.write(l.dumpZPL())
+#file.close()
+
+zpl = l.dumpZPL()
+w = "{:.2f}".format(round(l.width/25.4, 2))  
+h = "{:.2f}".format(round(l.height/25.4, 2)) 
+# adjust print density (8dpmm), label width (4 inches), label height (6 inches), and label index (0) as necessary
+url = f"http://api.labelary.com/v1/printers/12dpmm/labels/{h}x{w}/"
+open("/tmp/test.txt", "w").write(url)
+files = {'file' : zpl}
+headers = {'Accept' : 'application/pdf'} # omit this line to get PNG images back
+response = requests.post(url, headers = headers, files = files, stream = True)
+
+if response.status_code == 200:
+    response.raw.decode_content = True
+    with open('/tmp/label.pdf', 'wb') as out_file: # change file name for PNG images
+        shutil.copyfileobj(response.raw, out_file)
+
+
+
+#error = subprocess.run([
+#    "curl", "--request", "POST", 
+#    f"http://api.labelary.com/v1/printers/12dpmm/labels/{l.width/25.4}x{l.height/25.4}/0/", 
+#    "--form", "file=@/tmp/zpl_label.zpl", "--header", "\"Accept: application/pdf\"", ">", "/tmp/label.pdf"] )
+    #capture_output = True)
+
+#file = open("/tmp/label.pdf", "w")
+#file.write(str(error.stdout))
+#file.close()
